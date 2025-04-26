c2442671cbed37a2c3aff2739860c23ea1b59a58
Sergey Sorokotyagin <sv40@list.ru>
2025-04-26 14:38:13 +0300

    feat(daysteps.go): add func parsePackage

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,7 +15,27 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	//Разделить строку на слайс строк.
+	dataSl := strings.Split(data, ",")
+	//Проверить, чтобы длина слайса была равна 2, так как в строке данных у нас количество шагов и продолжительность.
+	if len(dataSl) != 2 {
+		return 0, 0, fmt.Errorf("ожидается строка формата '678,0h50m', получено: '%s'", data)
+	}
+	//Преобразовать первый элемент слайса (количество шагов) в тип int. Обработать возможные ошибки. При их возникновении из функции вернуть 0 шагов, 0 продолжительность и ошибку.
+	steps, err := strconv.Atoi(dataSl[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования шагов")
+	}
+	if steps == 0 {
+		return 0, 0, fmt.Errorf("0 шагов")
+	}
+	//Преобразовать второй элемент слайса в time.Duration. В пакете time есть метод для парсинга строки в time.Duration. Обработать возможные ошибки. При их возникновении из функции вернуть 0 шагов, 0 продолжительность и ошибку.
+	duration, err := time.ParseDuration(dataSl[1])
+	if err != 0 {
+		return 0, 0, fmt.Errorf("ошибка преобразования продолжительности")
+	}
+	//Если всё прошло без ошибок, верните количество шагов, продолжительность и nil (для ошибки).
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
